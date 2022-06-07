c2de926a7043f6d204e449fd9db5d5f0242b05da
Kishore Jagannath <kishorerj@google.com>
2022-06-07 17:52:06 +0530

    refactoring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from google.cloud import pubsub_v1
+import os
 
 def hello_gcs(event, context):
     """Triggered by a change to a Cloud Storage bucket.
@@ -16,8 +17,16 @@ def hello_gcs(event, context):
     print('Created: {}'.format(event['timeCreated']))
     print('Updated: {}'.format(event['updated']))
 
+    projectId = os.environ.get('PROJECT_ID')
+    if projectId is None:
+         print("ERROR project is null")
+    topicId = os.environ.get("TOPIC")
+    if topicId is None:
+         print("ERROR topic is null")
+    template_id = os.environ.get('TRANSCODE_TEMPLATE')
+    if template_id is None:
+         print("ERROR templateid is null")
     publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path("kishorerjbloom", "transfer-topic")
-
-    publisher.publish(topic_path, (event['bucket']+","+event['name']).encode("utf-8"))
+    topic_path = publisher.topic_path(projectId, topicId)
+    publisher.publish(topic_path, (event['bucket']+","+ event['name']+ ","+ template_id).encode("utf-8"))
     return 'OK'
